1bc34e52c39882126ed07136ab7f2e07f36186f6
cameron taylor <cameron.taylor.ninja@gmail.com>
2017-11-03 20:41:53 -0700

    cat anims added

--- source/Cat.hx
+++ source/Cat.hx
@@ -16,8 +16,8 @@ class Cat extends FlxSprite
 		
 		loadGraphic(AssetPaths.catSpriteSheet__png, true, 710, 429);
 		animation.add("punched", [0, 1], 12, false);
-		animation.add("peek", //2-31 to do and then frame 32(0 based) is flying anim
-		
+		animation.add("peek", [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30. 31], 24, false);
+		animation.add("fly", [32]);
 		
 	}
 	
