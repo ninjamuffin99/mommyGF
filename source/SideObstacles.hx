583be2bb89e3e5a28c1e82e47f3c1b0764373829
Cameron <cameron.taylor.ninja@gmail.com>
2017-11-14 23:46:37 -0800

    added side obstacle heheheh

---
+++ source/SideObstacles.hx
@@ -0,0 +1,21 @@
+package;
+
+import flixel.FlxSprite;
+import flixel.system.FlxAssets.FlxGraphicAsset;
+
+/**
+ * ...
+ * @author ninjaMuffin
+ */
+class SideObstacles extends FlxSprite 
+{
+
+	public function new(?X:Float=0, ?Y:Float=0, ?SimpleGraphic:FlxGraphicAsset) 
+	{
+		super(X, Y, SimpleGraphic);
+		
+		makeGraphic(100, 32);
+		
+	}
+	
+}
