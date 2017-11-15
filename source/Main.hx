d36832c4299a243ee5f2cf78192d2b36f3b806be
Cameron <cameron.taylor.ninja@gmail.com>
2017-11-15 01:42:41 -0800

    some new assets

--- source/Main.hx
+++ source/Main.hx
@@ -8,6 +8,6 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
-		addChild(new FlxGame(0, 0, GameOverState));
+		addChild(new FlxGame(0, 0, MenuState));
 	}
 }
--- source/MenuState.hx
+++ source/MenuState.hx
@@ -11,6 +11,23 @@ class MenuState extends FlxState
 {
 	override public function create():Void
 	{
+		var bg:FlxSprite = new FlxSprite();
+		bg.loadGraphic(AssetPaths.TitleScreen__png, false, 1927, 1080);
+		bg.setGraphicSize(1920, 1080);
+		bg.updateHitbox();
+		bg.setGraphicSize(Std.int(bg.width / 2));
+		bg.updateHitbox();
+		add(bg);
+		
+		var _titleText:FlxSprite = new FlxSprite(0, FlxG.height * 0.04);
+		_titleText.loadGraphic(AssetPaths.titleTextsheet__png, true, 1120, 452);
+		_titleText.setGraphicSize(Std.int(_titleText.width / 2));
+		_titleText.updateHitbox();
+		_titleText.screenCenter(X);
+		_titleText.animation.add("idle", [0, 1, 2], 12);
+		_titleText.animation.play("idle");
+		add(_titleText);
+		
 		super.create();
 	}
 
