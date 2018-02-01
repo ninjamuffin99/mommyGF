081b4db8fb336449cc2fdf7adea0e3a8f45d6f1a
Cameron <cameron.taylor.ninja@gmail.com>
2018-02-01 11:02:17 -0800

    made menu selectable

--- source/MenuState.hx
+++ source/MenuState.hx
@@ -17,6 +17,9 @@ class MenuState extends FlxState
 	private var _titleChallenges:FlxSprite;
 	private var _titleOptions:FlxSprite;
 	private var _titleCredits:FlxSprite;
+	private var _selector:FlxSprite;
+	
+	private var selectorPos:Int = 0;
 
 	override public function create():Void
 	{
@@ -45,6 +48,14 @@ class MenuState extends FlxState
 		
 		createMenu();
 		
+		_selector = new FlxSprite(25, 20);
+		_selector.loadGraphic("assets/images/Selector.png", false, 99, 166);
+		_selector.setGraphicSize(Std.int(_selector.width / 2));
+		_selector.updateHitbox();
+		add(_selector);
+		
+		_selector.x = _titleStart.x - 25;
+		_selector.y = _titleStart.y;
 		
 		
 		super.create();
@@ -58,48 +69,96 @@ class MenuState extends FlxState
 		_titleStart.updateHitbox();
 		_titleStart.screenCenter(X);
 		add(_titleStart);
-
+		
 		_titleChallenges = new FlxSprite(0, FlxG.height * 0.55);
 		_titleChallenges.loadGraphic(AssetPaths.titleChallenge__png, false, 788, 153);
 		_titleChallenges.setGraphicSize(Std.int(_titleChallenges.width / 2));
 		_titleChallenges.updateHitbox();
 		_titleChallenges.screenCenter(X);
 		add(_titleChallenges);
-
+		
 		_titleOptions = new FlxSprite(0, FlxG.height * 0.7);
 		_titleOptions.loadGraphic(AssetPaths.titleOptions__png, false, 652, 165);
 		_titleOptions.setGraphicSize(Std.int(_titleOptions.width / 2));
 		_titleOptions.updateHitbox();
 		_titleOptions.screenCenter(X);
 		add(_titleOptions);
-
+		
 		_titleCredits = new FlxSprite(0, FlxG.height * 0.84);
 		_titleCredits.loadGraphic(AssetPaths.titleCredits__png, false, 530, 150);
 		_titleCredits.setGraphicSize(Std.int(_titleCredits.width / 2));
 		_titleCredits.updateHitbox();
 		_titleCredits.screenCenter(X);
 		add(_titleCredits);
-
 	}
 
 	override public function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
-
+		
 		#if !mobile
-		if (FlxG.keys.justPressed.ANY)
+		if (FlxG.keys.anyJustPressed(["UP", "W", "I", "DOWN", "S", "K"]))
 		{
-			FlxG.switchState(new PlayState());
+			if (FlxG.keys.anyJustPressed(["UP", "W", "I"]))
+			{
+				selectorPos -= 1;
+			}
+			else if (FlxG.keys.anyJustPressed(["DOWN", "S", "K"]))
+			{
+				selectorPos += 1;
+			}
+			
+			var selectorOffset:Float = 50;
+			switch (selectorPos) 
+			{
+				case 1:
+					_selector.x = _titleChallenges.x - selectorOffset;
+					_selector.y = _titleChallenges.y;
+				case 2: 
+					_selector.x = _titleOptions.x - selectorOffset;
+					_selector.y = _titleOptions.y;
+				case 3:
+					_selector.x = _titleCredits.x - selectorOffset;
+					_selector.y = _titleCredits.y;
+				default:
+					// is 0 pos, refering to starting i guess
+					_selector.x = _titleStart.x - selectorOffset;
+					_selector.y = _titleStart.y;
+			}
+			
 		}
+		
 		#end
-
+		
+		if (selectorPos < 0)
+			selectorPos = 3;
+		if (selectorPos > 3)
+			selectorPos = 0;
+		
+		if (FlxG.keys.anyJustPressed(["SPACE", "ENTER"]))
+		{
+			switch (selectorPos) 
+			{
+				case 0:
+					FlxG.switchState(new PlayState());
+				case 1:
+					FlxG.log.add("Selected Challenges");
+				case 2: 
+					FlxG.log.add("Options Slected");
+				case 3:
+					FlxG.log.add("Credits Selected");
+				default:
+					FlxG.log.add("Defualt selection???");
+			}
+		}
+		
 		for (touch in FlxG.touches.list)
 		{
 			if (touch.justPressed)
 			{
 				FlxG.switchState(new PlayState());
 			}
-
+			
 		}
 
 	}
