c59520b44cb4b963c9a93007caacd2c6fab5b562
Cameron <cameron.taylor.ninja@gmail.com>
2018-02-21 21:46:03 -0800

    simplified creds state and got it going

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -1,6 +1,8 @@
 package;
 
+import flixel.FlxG;
 import flixel.FlxState;
+import flixel.text.FlxText;
 
 /**
  * ...
@@ -8,86 +10,39 @@ import flixel.FlxState;
  */
 class CreditsState extends FlxState 
 {
-	public var creds:Array<String> = 
-	[
-		"Created by PhantomArcade and Cameron Taylor aka NinjaMuffin99",
-		"In association with Newgrounds.com, created by Tom Fulp",
-		"Designed by NinjaMuffin99 and PhantomArcade",
-		"Programming by NinjaMuffin99",
-		"Art by PhantomArcade",
-		"Shoutouts to some of these testers/pals",
-		"THE ONE AND ONLY FuShark",
-		"BrandyBuizel",
-		"Cameron Talbot",
-		"Cory/Spazkid",
-		"Cameron would like to thank/shoutout... (in no particular order)",
-		"PhantomArcade lol ayyy",
-		"Tom Fulp",
-		"That picture of Tom where he's wearing a turtleneck sweater",
-		"Especially that one",
-		"Literally everyone on Newgrounds.com",
-		"Especially the people who upload stuff to this day",
-		"FuShark (we still need to make that hentai game dude)",
-		"Cameron Talbot",
-		"BrandyBuizel",
-		"EiGi",
-		"Moawling",
-		"StuffedWombat",
-		"DigitalFudge",
-		"aninvisiblepirate",
-		"TheOverGhoul",
-		"Troisnyx",
-		"MSGHero",
-		"PsychoGoldFish",
-		"RealFaction",
-		"LucidShadowDreamer",
-		"Tanner Monkman",
-		"Brett Procee",
-		"Ryan Dugaro",
-		"Paol from CFEC",
-		"Charles from CFEC",
-		"The staff of The Youth Zone",
-		"Other staff from CFEC",
-		"The guys who go to Caribou Men's Night",
-		"Marvin Ryan Sr.",
-		"Darcy Ryan",
-		"The HaxeFlixel Community, specifically the Discord channel",
-		"Indie Game The Movie",
-		"Adam Saltsman",
-		"Ryan Koonz",
-		"Nicky Case",
-		"Sam Barlow",
-		"Nina Freeman",
-		"The guy who made Emily Is Away oops shoutouts to him",
-		"Everyone who follows me on Twitter ayyy @ninja_muffin99 yall",
-		"Also everyone who follows me on Newgrounds ayyyy ninjamuffin99 over there lmaooo",
-		"Shoutouts to SimpleFlips",
-		"Mark Gould",
-		"Mason Gould",
-		"Jacob Lawrence",
-		"Ben Lawrence",
-		"SuperMegaCast",
-		"SleepyCast",
-		"LazyCast",
-		"RebelTaxi Pizza Party Podcast",
-		"Accidental Queens",
-		"Davey Wreden",
-		"/r/gamedev lol SYKE THEY SUCK LMAOOOOO",
-		"Studio MDHR",
-		"Bennett Foddy",
-		"TylerNG",
-		"Tyler Glaiel",
-		"The Behemoth",
-		"The video game Cities: Skylines",
-		"Loodumb Darray",
-		"Metanet Software",
-		"Alex Rose",
-		"Thekla"
-	];
-
-	public function new() 
+	
+	private var _txtCreds:FlxText;
+	
+	override public function create():Void 
 	{
+		_txtCreds = new FlxText(0, FlxG.height, 0, "", 16);
+		add(_txtCreds);
+		
+		for (i in 0...creds.length)
+		{
+			_txtCreds.text += creds[i] + "\n";
+		}
+		
+		super.create();
+	}
+	
+	override public function update(elapsed:Float):Void 
+	{
+		super.update(elapsed);
+		
+		if (FlxG.keys.justPressed.ANY)
+		{
+			FlxG.switchState(new MenuState());
+		}
 		
 	}
+	
+	public var creds:Array<String> = 
+	[
+		"Created by PhantomArcade and NinjaMuffin99",
+		"For Newgrounds.com",
+		"lov u tom fulp"
+	];
+
 	
 }
