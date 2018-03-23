93c948292cb259df7eb3092c7f2e81f3bb04748b
Cameron <cameron.taylor.ninja@gmail.com>
2018-03-22 21:21:46 -0700

    got map panning and shit going

--- source/LevelSelect.hx
+++ source/LevelSelect.hx
@@ -1,6 +1,7 @@
 package;
 
 import flixel.FlxG;
+import flixel.FlxObject;
 import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.text.FlxText;
@@ -16,20 +17,46 @@ class LevelSelect extends FlxState
 	private var _selector:FlxSprite;
 	private var selectorPos:Int = 0;
 	
-	override public function create():Void 
+	private var _map:FlxSprite;
+	private var _camTrack:FlxObject;
+	
+	override public function create():Void
 	{
+		_map = new FlxSprite(0, 0);
+		_map.loadGraphic(AssetPaths.map__png, false, 2400, 1350);
+		add(_map);	
+		
+		for (i in 1...3)
+		{
+			var levelLabel:FlxSprite = new FlxSprite(200, (i * 120) + 200).loadGraphic("assets/images/level_label_" + i + ".png", false, 281, 102);
+			levelLabel.scrollFactor.set();
+			levelLabel.scale.set(2, 2);
+			levelLabel.updateHitbox();
+			add(levelLabel);
+		}
+		
 		levels = new FlxText(200, 200, 0, "Home\nOutside", 64);
+		levels.scrollFactor.set();
 		add(levels);
 		
 		_selector = new FlxSprite(25, 20);
 		_selector.loadGraphic("assets/images/Selector.png", false, 99, 166);
 		_selector.setGraphicSize(Std.int(_selector.width / 2));
 		_selector.updateHitbox();
+		_selector.scrollFactor.set();
 		add(_selector);
 		
 		_selector.x = levels.x - 50;
 		_selector.y = levels.y;
 		
+		
+		FlxG.camera.zoom = 0.5;
+		_camTrack = new FlxObject(0, 0, 16, 16);
+		add(_camTrack);
+		
+		FlxG.camera.follow(_camTrack);
+		FlxG.camera.followLerp = 0.04;
+		FlxG.camera.setScrollBounds(0, _map.width, 0, _map.height);
 		
 		super.create();
 	}
@@ -52,18 +79,22 @@ class LevelSelect extends FlxState
 			}
 			
 			if (selectorPos < 0)
-				selectorPos = 1;
-			if (selectorPos > 1)
+				selectorPos = 2;
+			if (selectorPos > 2)
 				selectorPos = 0;
 			
 			var selectorOffset:Float = 50;
 			switch (selectorPos) 
 			{
 				case 0:
-					_selector.y = levels.y;
+					_camTrack.setPosition(800, 170);
 				case 1:
-					_selector.y = levels.y + 66;
+					_camTrack.setPosition(1500, 170);
+				case 2:
+					
 			}
+			
+			_selector.y = (selectorPos * 320) + 320;
 			
 		}
 		
@@ -95,6 +126,7 @@ class LevelSelect extends FlxState
 		{
 			for (touch in FlxG.touches.list)
 			{
+				/*
 				if (touch.y > levels.y + 66)
 				{
 					FlxG.switchState(new OutsideState());
@@ -103,6 +135,7 @@ class LevelSelect extends FlxState
 				{
 					FlxG.switchState(new PlayState());
 				}
+				*/
 			}
 		}
 	}
