a1204382ccec59e7cf18f4ded490b687ff113604
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2018-09-09 17:38:37 -0700

    getting options screen fixed up

--- source/SubstateOptions.hx
+++ source/SubstateOptions.hx
@@ -5,8 +5,8 @@ import flixel.FlxSprite;
 import flixel.FlxSubState;
 import flixel.addons.display.FlxTiledSprite;
 import flixel.graphics.frames.FlxAtlasFrames;
+import flixel.group.FlxGroup.FlxTypedGroup;
 import flixel.util.FlxColor;
-import openfl.display.BlendMode;
 
 /**
  * ...
@@ -17,12 +17,21 @@ class SubstateOptions extends FlxSubState
 	
 	private var scrollingHeads:FlxTiledSprite;
 	
+	public static var masterVol:Float = 1;
+	public static var musicVol:Float = 1;
+	public static var soundVol:Float = 1;
+	public static var antialias:Bool = true;
+	
+	private var curSelected:Int = 0;
+	
 	private var menuItems:Array<Dynamic> = 
 	[
-		["masterVol", "musicVol", "sfx", "antialias"],
-		["slider", "slider", "slider", "checkbox"]
+		["masterVol", "musicVol", "sfx", "antialiasing"],
+		["slider", "slider", "slider", "checkBox"]
 		
 	];
+	
+	private var grpMenuItems:FlxTypedGroup<FlxSprite>;
 
 	public function new(BGColor:FlxColor=FlxColor.TRANSPARENT) 
 	{
@@ -36,29 +45,65 @@ class SubstateOptions extends FlxSubState
 		add(blueShit);
 		
 		scrollingHeads = new FlxTiledSprite(AssetPaths.HeadPattern__png, 968, 540);
-		scrollingHeads.blend = "lighten";
+		// scrollingHeads.blend = "lighten";
 		add(scrollingHeads);
 		scrollingHeads.alpha = 0.6;
 		scrollingHeads.scrollY = 50;
 		
-		
 		var optTex = FlxAtlasFrames.fromSpriteSheetPacker(AssetPaths.optionsSheet__png, AssetPaths.optionsSheet__txt);
 		
-		var optionsTitle:FlxSprite = new FlxSprite();
+		var optionsTitle:FlxSprite = new FlxSprite(0, 20);
 		optionsTitle.frames = optTex;
+		optionsTitle.animation.add("idle", [0, 2], 12);
+		optionsTitle.animation.play("idle");
+		optionsTitle.screenCenter(X);
 		add(optionsTitle);
+		
+		grpMenuItems = new FlxTypedGroup<FlxSprite>();
+		add(grpMenuItems);
 		
 		for (i in 0...menuItems[0].length)
 		{
-			var menuShit:FlxSprite = new FlxSprite(
+			var menuShit:FlxSprite = new FlxSprite(20, 130 + 80 * i);
+			var tex = FlxAtlasFrames.fromSpriteSheetPacker("assets/images/UI/options/" + menuItems[0][i] + "Sheet.png", "assets/images/UI/options/" + menuItems[0][i] +"Sheet.txt");
+			menuShit.frames = tex;
+			menuShit.animation.add("idle", [0]);
+			menuShit.animation.add("selected", [1, 2, 3, 4], 12);
+			menuShit.animation.play("idle");
+			grpMenuItems.add(menuShit);
+			
+			FlxG.log.add(i);
+			
+			var menuSel:FlxSprite = new FlxSprite(400, 150 + 80 * i);
+			tex = FlxAtlasFrames.fromSpriteSheetPacker("assets/images/UI/options/" + menuItems[1][i] + "Sheet.png", "assets/images/UI/options/" + menuItems[1][i] +"Sheet.txt");
+			menuSel.frames = tex;
+			add(menuSel);
 		}
-		
 	}
 	
-	override public function update(elapsed:Float):Void 
+	override public function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
+		
+		if (FlxG.keys.anyJustPressed(["DOWN", "S", "K"]))
+			curSelected += 1;
+		else if (FlxG.keys.anyJustPressed(["UP", "W", "I"]))
+			curSelected -= 1;
+		
+		if (curSelected < 0)
+			curSelected = Std.int(menuItems[0].length - 1);
+		else if (curSelected > menuItems[0].length - 1)
+			curSelected = 0;
 	
+		grpMenuItems.forEach(function(spr:FlxSprite)
+		{
+			spr.animation.play("idle");
+		});	
+		
+		grpMenuItems.members[curSelected].animation.play("selected");
+			
+		FlxG.watch.addQuick("curSel", curSelected);
+		
 		var scrollShit:Float = FlxG.height * 0.4 * 0.25 * FlxG.elapsed;
 		
 		scrollingHeads.scrollY += scrollShit;
